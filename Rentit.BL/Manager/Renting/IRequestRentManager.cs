5520fbe76617ec7b37293c2b14fd9cd197fa178c
Mohamed abd elaziz <mohamedabdelaziz2812@gmail.com>
2024-03-23 11:08:08 +0200

    Authorization error  resolved

--- Rentit.BL/Manager/Renting/IRequestRentManager.cs
+++ Rentit.BL/Manager/Renting/IRequestRentManager.cs
@@ -13,8 +13,6 @@ namespace Rentit.BL
         IEnumerable<RequestRentReadDto> GetAllForAdmin();
         IEnumerable<RequestRentReadDto> GetAllForUser(int id);
         IEnumerable<RequestRentReadDto> GetAllForHost(int id);
-      
-
         bool AddRequest (RequestRentAddDto item , int propertyid); 
         bool AcceptByHost (int propertyid);
         bool CancelByHost(int propertyid);
